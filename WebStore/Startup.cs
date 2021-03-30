9a9539c7d3d1313387f3015f9c259e481df0ff76
Nikolai Glebanov <n.glebanov@gmail.com>
2021-03-30 16:16:06 +0300

    Динамическое конфигурирование приложения

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,17 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebStore
 {
-    public class Startup
-    {
+    public record Startup(IConfiguration Configuration)
+    { 
+        //private IConfiguration Configuration { get; }
+        //public Startup(IConfiguration Configuration)
+        //{
+        //    this.Configuration = Configuration;
+        //}
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -26,11 +28,14 @@ namespace WebStore
 
             app.UseRouting();
 
+            //var greetings = Configuration["Greetings"];
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    //await context.Response.WriteAsync(greetings);
+                    await context.Response.WriteAsync(Configuration["Greetings"]);
                 });
             });
         }
