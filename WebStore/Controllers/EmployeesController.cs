f344617eb44dc2112d13b0ce958f3dbc0c24e35b
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-02 11:19:48 +0300

    Макет

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Data;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private List<Employee> _Employees;
+
+        public EmployeesController()
+        {
+            _Employees = TestData.Employees;
+
+        }
+
+        public IActionResult Index() => View(_Employees);
+
+        public IActionResult Details(int id) // http://localhost:5000/employees/details/2
+        {
+            var employee = _Employees.FirstOrDefault(e => e.Id == id);
+            if (employee is not null)
+                return View(employee);
+            return NotFound();
+        }
+    }
+}
