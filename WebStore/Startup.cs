17e4cc8996fadf3ea6d16589d5b0c0a0eb57f551
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-03 23:21:30 +0300

    Промежуточное ПО

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebStore.Infrastructure.Middleware;
 
 namespace WebStore
 {
@@ -29,7 +30,19 @@ namespace WebStore
             }
 
             app.UseStaticFiles();
+
             app.UseRouting();
+
+            app.UseWelcomePage("/welcome");
+
+            app.UseMiddleware<TestMiddleware>();
+
+            app.MapWhen(
+                context => context.Request.Query.ContainsKey("id") && context.Request.Query["id"] == "5",
+                context => context.Run(async request => await request.Response.WriteAsync("Hello with id ==5!"))
+                );
+            app.Map("/hello", context => context.Run(async request => await request.Response.WriteAsync("Hello!!!")));
+
 
             app.UseEndpoints(endpoints =>
           {
