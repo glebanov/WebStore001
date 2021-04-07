37fc65b30f3030d0538e00cc00b52979ad3a8d7f
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-07 15:29:06 +0300

    Доопределение сущностей для использования их в контексте БД EFCore

--- WebStore.Domain/Entities/Base/Entity.cs
+++ WebStore.Domain/Entities/Base/Entity.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class Entity : IEntity
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Можно не писать
         public int Id { get; set; }
     }
 }
--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    //[Table("Brands")] //- Добавляем таблицу, где будет храниться набор сущностей
     public class Brand : NamedEntity, IOrderedEntity
     {
+        //[Column("BrandOrder")] //- Можно указать из какого столбца данные будут загружены
         public int Order { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
