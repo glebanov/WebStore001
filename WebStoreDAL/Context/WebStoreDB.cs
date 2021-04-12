5f9f57f72dbab46149b459a2099ca38df23f3380
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-12 16:21:00 +0300

    Миграция БД для добавления туда заказов

--- WebStoreDAL/Context/WebStoreDB.cs
+++ WebStoreDAL/Context/WebStoreDB.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Domain.Entities.Orders;
 
 namespace WebStore.DAL.Context
 {
@@ -12,6 +13,8 @@ namespace WebStore.DAL.Context
         public DbSet<Brand> Brands { get; set; }
 
         public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
 
         public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options) { }
     }
