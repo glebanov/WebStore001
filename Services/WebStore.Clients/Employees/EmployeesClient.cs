cacd7aad42254ff086750411698b1ff82573dc5b
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-18 19:19:20 +0300

    Есть клиент для работы с WebAPI управления сотрудниками

--- Services/WebStore.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.Clients/Employees/EmployeesClient.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Net.Http;
 using WebStore.Clients.Base;
+using WebStore.Domain.Models;
 using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.Clients.Employees
 {
-    public class EmployeesClient : BaseClient
+    public class EmployeesClient : BaseClient, IEmployeesData
     {
         public EmployeesClient(IConfiguration Configuration) : base(Configuration, WebAPI.Employees) { }
+
+
+        public IEnumerable<Employee> Get() => Get<IEnumerable<Employee>>(Address);
+
+        public Employee Get(int id) => Get<Employee>($"{Address}/{id}");
+
+        public Employee GetByName(string LastName, string FirstName, string Patronymic) =>
+            Get<Employee>($"{Address}/employee?LastName={LastName}&FirstName={FirstName}&Patronymic={Patronymic}");
+
+        public int Add(Employee employee) => Post(Address, employee).Content.ReadAsAsync<int>().Result;
+
+        public Employee Add(string LastName, string FirstName, string Patronymic, int Age) =>
+            Post($"{Address}/employee?LastName={LastName}&FirstName={FirstName}&Patronymic={Patronymic}", "")
+               .Content.ReadAsAsync<Employee>().Result;
+
+        public void Update(Employee employee) => Put(Address, employee);
+
+        public bool Delete(int id) => Delete($"{Address}/{id}").IsSuccessStatusCode;
     }
 }
