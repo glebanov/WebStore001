ec034577c2ae7aacd90428785a19a7b78cd445f9
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-18 20:15:22 +0300

    DTO-модели для работы WebAPI каталога товаров

--- Services/WebStore.Interfaces/Services/IProductData.cs
+++ Services/WebStore.Interfaces/Services/IProductData.cs
@@ -9,6 +9,7 @@ namespace WebStore.Interfaces.Services
         IEnumerable<Section> GetSections();
 
         IEnumerable<Brand> GetBrands();
+
         IEnumerable<Product> GetProducts(ProductFilter Filter = null);
 
         Product GetProductById(int id);
