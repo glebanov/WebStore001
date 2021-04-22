caa385ad31a29ae0ede71f8b2cfd121b8771e044
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-22 12:13:03 +0300

    Исправления

--- UI/WebStore/Controllers/CartController.cs
+++ UI/WebStore/Controllers/CartController.cs
@@ -61,6 +61,7 @@ namespace WebStore.Controllers
                 Order = OrderModel,
                 Items = _CartServices.GetViewModel().Items.Select(item => new OrderItemDTO
                 {
+                    Id = item.Product.Id,
                     Price = item.Product.Price,
                     Quantity = item.Quantity,
                 }).ToList()
