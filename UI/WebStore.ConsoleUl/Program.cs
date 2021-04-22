1711761782ff7f2f5f9db56d6fbdebd8fb9ec9bc
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-22 21:30:23 +0300

    Тестовый консольный клиент

---
+++ UI/WebStore.ConsoleUl/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Products;
+
+namespace WebStore.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var products_client = new ProductsClient(configuration);
+
+            Console.WriteLine("К запросу готов!");
+            Console.ReadLine();
+
+            foreach (var product in products_client.GetProducts())
+                Console.WriteLine("{0} - {1}", product.Name, product.Price);
+
+            Console.WriteLine("Запрос завершён");
+            Console.ReadLine();
+        }
+    }
+}
