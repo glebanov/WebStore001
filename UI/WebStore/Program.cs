01d03124394982614a3297c7a07a0f0a027aea04
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-27 14:12:51 +0300

    Инфраструктура логирования на основе бибилотеки log4net

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 namespace WebStore
 {
     public class Program
