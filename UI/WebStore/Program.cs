eb5c6cfcfe892655ded0859872f62cb346979036
Nikolai Glebanov <n.glebanov@gmail.com>
2021-04-29 14:30:50 +0300

    Файлы конфигурации log4net

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,32 +1,32 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 namespace WebStore
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-       Host.CreateDefaultBuilder(args)
-         .ConfigureLogging((host, log) => log
+            Host.CreateDefaultBuilder(args)
+                //.ConfigureLogging((host, log) => log
+                //   .ClearProviders()
+                //   .AddEventLog()
+                //   .AddConsole()
+                //   .AddFilter/*<ConsoleLoggerProvider>*/("Microsoft.Hosting", LogLevel.Error)
+                //   .AddFilter((category, level) => !(category.StartsWith("Microsoft") && level >= LogLevel.Warning))
+                //)
+                .ConfigureLogging((host, log) => log
                    //.ClearProviders()
                    //.AddEventLog()
                    .AddConsole(opt => opt.IncludeScopes = true)
                 //.AddFilter/*<ConsoleLoggerProvider>*/("Microsoft.Hosting", LogLevel.Error)
                 //.AddFilter((category, level) => !(category.StartsWith("Microsoft") && level >= LogLevel.Warning))
                 )
-           .ConfigureWebHostDefaults(host => host
-              .UseStartup<Startup>()
-           );
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
+                .ConfigureWebHostDefaults(host => host
+                   .UseStartup<Startup>()
+                )
+            ;
     }
 }
