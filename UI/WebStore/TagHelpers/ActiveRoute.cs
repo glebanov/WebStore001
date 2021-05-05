4a6fecc746ad3d367e8aec06f3e9af17d8a38485
Nikolai Glebanov <n.glebanov@gmail.com>
2021-05-05 12:30:42 +0300

    Заготовка таг-хелпера

---
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(Attributes = AttributeName)]
+    public class ActiveRoute : TagHelper
+    {
+        private const string AttributeName = "is-active-route";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+
+            output.Attributes.RemoveAll(AttributeName);
+        }
+    }
+}
