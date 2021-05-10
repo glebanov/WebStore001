42d70efd29cfa62b8ee1922cfa3f34571935961f
Nikolai Glebanov <n.glebanov@gmail.com>
2021-05-10 14:08:53 +0300

    Revert "Переделываем таг-хелпер генерации переключалки страниц"
    This reverts commit bb426bbdd33d1e08a1739a1ea8d4713e3aae7639.

--- UI/WebStore/TagHelpers/Paging.cs
+++ UI/WebStore/TagHelpers/Paging.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
@@ -12,7 +11,7 @@ namespace WebStore.TagHelpers
 {
     public class Paging : TagHelper
     {
-       // private readonly IUrlHelperFactory _UrlHelperFactory;
+        private readonly IUrlHelperFactory _UrlHelperFactory;
 
         [ViewContext, HtmlAttributeNotBound]
         public ViewContext ViewContext { get; set; }
@@ -24,37 +23,31 @@ namespace WebStore.TagHelpers
         [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
         public Dictionary<string, object> PageUrlValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
 
-      //  public Paging(IUrlHelperFactory UrlHelperFactory) => _UrlHelperFactory = UrlHelperFactory;
+        public Paging(IUrlHelperFactory UrlHelperFactory) => _UrlHelperFactory = UrlHelperFactory;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var ul = new TagBuilder("ul");
             ul.AddCssClass("pagination");
 
-          //  var url_helper = _UrlHelperFactory.GetUrlHelper(ViewContext);
+            var url_helper = _UrlHelperFactory.GetUrlHelper(ViewContext);
             for (var i = 1; i <= PageModel.TotalPages; i++)
-                ul.InnerHtml.AppendHtml(CreateElement(i/*, url_helper*/));
+                ul.InnerHtml.AppendHtml(CreateElement(i, url_helper));
 
             output.Content.AppendHtml(ul);
         }
 
-        private TagBuilder CreateElement(int PageNumber/*, IUrlHelper Url*/)
+        private TagBuilder CreateElement(int PageNumber, IUrlHelper Url)
         {
             var li = new TagBuilder("li");
             var a = new TagBuilder("a");
 
             if (PageNumber == PageModel.Page)
-            {
                 li.AddCssClass("active");
-                a.MergeAttribute("data-page", PageNumber.ToString());
-            }
             else
             {
                 PageUrlValues["page"] = PageNumber;
-                //a.Attributes["href"] = Url.Action(PageAction, PageUrlValues);
-                a.Attributes["href"] = "#";
-                foreach (var (key, value) in PageUrlValues.Where(v => v.Value is { }))
-                    a.MergeAttribute($"data-{key}", value.ToString());
+                a.Attributes["href"] = Url.Action(PageAction, PageUrlValues);
             }
 
             a.InnerHtml.AppendHtml(PageNumber.ToString());
