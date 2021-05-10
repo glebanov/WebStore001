966951e097e8755c40a96445c1e795bfe446c7dc
Nikolai Glebanov <n.glebanov@gmail.com>
2021-05-10 11:05:11 +0300

    Визуальная часть постраничного разбиения каталога товаров

--- Common/WebStore.Domain/ViewModels/ProductViewModel.cs
+++ Common/WebStore.Domain/ViewModels/ProductViewModel.cs
@@ -1,15 +1,33 @@
-﻿namespace WebStore.Domain.ViewModels
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebStore.Domain.ViewModels
 {
-    public record ProductViewModel
+    public class ProductViewModel
     {
-        public int Id { get; init; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class CatalogViewModel
+    {
+        public IEnumerable<ProductViewModel> Products { get; set; }
+        public int? SectionId { get; set; }
 
-        public string Name { get; init; }
+        public int? BrandId { get; set; }
 
-        public string ImageUrl { get; init; }
+        public PageViewModel PageViewModel { get; set; }
+    }
 
-        public decimal Price { get; init; }
+    public class PageViewModel
+    {
+        public int Page { get; set; }
 
-        public string Brand { get; init; }
+        public int PageSize { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public int TotalPages => PageSize == 0 ? 0 : (int)Math.Ceiling((double)TotalItems / PageSize);
     }
 }
