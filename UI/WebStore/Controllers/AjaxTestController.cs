3d5088f91b7f00e83209f5864effc02369626aaf
Nikolai Glebanov <n.glebanov@gmail.com>
2021-05-10 20:08:18 +0300

    Чат на SignalR

--- UI/WebStore/Controllers/AjaxTestController.cs
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -26,5 +26,6 @@ namespace WebStore.Controllers
 
             return PartialView("Partial/_DataView", new AjaxTestDataViewModel(id, msg, DateTime.Now));
         }
+        public IActionResult SignalRTest() => View();
     }
 }
